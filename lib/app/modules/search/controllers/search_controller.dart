52a05f3bacc4542e1220c981361656d271e00a94
Xnnms <ilhamgallagher633@gmail.com>
2022-07-28 16:50:03 +0700

    comit app

---
+++ lib/app/modules/search/controllers/search_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+
+class SearchController extends GetxController {
+  // ignore: todo
+  //TODO: Implement SearchController
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+  void increment() => count.value++;
+}
